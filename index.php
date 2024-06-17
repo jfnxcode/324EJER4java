5513dcb6944561e11fd980a65b2bb5c5163e8353
Jorge A. Calderon <67562342+jfnxcode@users.noreply.github.com>
2024-06-17 07:50:38 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div style=" display:flex; align-content:center">
+    <h1>C#</h1>
+        <form action="https://localhost:44346/WebForm1.aspx" method="POST">
+            <label for="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre"><br><br>
+            <label for="apellido">apellido:</label>
+            <input type="text" id="apellido" name="apellido"><br><br>
+            <input type="submit" value="Submit">
+        </form>
+        <h1>JAVA</h1>
+        <form action="http://localhost:8083/E42A/NewServlet" method="POST">
+            <label for="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre"><br><br>
+            <label for="apellido">apellido:</label>
+            <input type="text" id="apellido" name="apellido"><br><br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+        
+</body>
+</html>
